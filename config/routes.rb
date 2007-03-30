d2cf1d88ef970fa9917b4243c9aec37f7c92f12f
Kjue <Kjue@e7f6307b-f62b-0410-b343-eef5a4bb3a65>
2007-03-30 09:57:00 +0000

    Initial migrations and models. Implementation pending on ~everything.

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,12 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
+
+  # RESTful routes
+  map.resources :users, :sessions
+  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate'
+
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.login  '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
 end
