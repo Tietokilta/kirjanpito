76f2b31f46b4508a0088495112c6398d65d51e9f
akolehma <akolehma@e7f6307b-f62b-0410-b343-eef5a4bb3a65>
2007-04-17 14:28:11 +0000

    Simple budget list

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,5 +2,15 @@ class Budget < ActiveRecord::Base
   has_many    :budget_accounts
 
   belongs_to  :fiscal_period
+	
+	
+	validates_presence_of :description, :fiscal_period_id
   
+
+	def validate
+		errors.add_to_base('The given fiscal period does not exist.') if fiscal_period_id.nil?
+	end
+
+
+
 end
