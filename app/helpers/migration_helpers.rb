30d5bd6220e16fac286ad537d3f33558132e0d1d
Kjue <Kjue@e7f6307b-f62b-0410-b343-eef5a4bb3a65>
2007-04-18 19:58:35 +0000

    On delete cascade through children.

--- app/helpers/migration_helpers.rb
+++ app/helpers/migration_helpers.rb
@@ -4,8 +4,9 @@ module MigrationHelpers
     
     execute %{alter table #{from_table}
               add constraint #{constraint_name}
-              foreign key (#{from_column})
-              references #{to_table}(id)}
+                  foreign key (#{from_column})
+                  references #{to_table}(id)
+                  ON DELETE CASCADE}
   end
 
   def drop_foreign_key(from_table, from_column, to_table)
