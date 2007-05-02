8da81fedef1c299bb3656ae97b854f6e2ce0da78
Kjue <Kjue@e7f6307b-f62b-0410-b343-eef5a4bb3a65>
2007-05-02 19:29:22 +0000

    Active Scaffold installed to the project. AccountTypes and Entries are now done with ActiveScaffold. No overrides done anywhere yet but apparent need is imminent.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,71 +1,92 @@
 class EntriesController < ApplicationController
-  def index
-    list
-    render :action => 'list'
+
+  active_scaffold :entry do |config|
+    config.columns = [:receipt_number, :description, :sum, :date, :fiscal_period.to_s, :debet_account, :credit_account]
+#    config.columns[:phone_number].description = "(Format: ###-###-####)"
+#    config.columns[:phone_number].label = "Phone"
+
+    config.create.columns.exclude :receipt_number
+    config.update.columns.exclude :receipt_number
+#    config.list.columns.exclude :first_name, :middle_name, :last_name
+#    config.subform.columns = [:first_name, :last_name, :login, :password]
+
+    config.list.sorting = {:date => 'ASC'}
+
+#    config.nested.add_link "Names", [:aliases]
+
+#    config.create.columns.exclude(:first_name, :middle_name, :last_name, :phone_number)
+#    config.create.columns.add_subgroup "Optional" do |group|
+#      group.add(:first_name, :middle_name, :last_name, :phone_number)
+#    end
   end
 
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
-
-  def list
-    @entry_pages, @entries = paginate :entries, :per_page => 10
-  end
-
-  def show
-    @entry = Entry.find(params[:id])
-  end
-
-  def new
-		#TODO: fetch fiscal period from somewhere
-		@entry = Entry.find(:first, :order => "id desc", :conditions => ['fiscal_period_id = ?', 2006])
-		@entry.id += 1
-		@entry.receipt_number += 1
-  end
-
-  def create
-    @entry = Entry.new(params[:entry])
-
-    if @entry.save
-      flash[:notice] = 'Entry was successfully created.'
-      #redirect_to :action => 'list'
-      redirect_to :action => 'new'
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @entry = Entry.find(params[:id])
-  end
-
-  def update
-    @entry = Entry.find(params[:id])
-    if @entry.update_attributes(params[:entry])
-      flash[:notice] = 'Entry was successfully updated.'
-      redirect_to :action => 'show', :id => @entry
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    Entry.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
-
-	def autocomplete_debet_account
-		data = params[:debet_account]
-		@accounts = Account.find(:all,
-				:conditions => [ 'number LIKE ? OR name LIKE ?', '%'+data+'%', '%'+data+'%'] )
-		render :inline => "<%= indexed_auto_complete_result @accounts, 'entry_debet_account_id', 'name', 'id' %>" 
-	end
-
-	def autocomplete_credit_account
-		data = params[:credit_account]
-		@accounts = Account.find(:all,
-				:conditions => [ 'number LIKE ? OR name LIKE ?', '%'+data+'%', '%'+data+'%'] )
-		render :inline => "<%= indexed_auto_complete_result @accounts, 'entry_credit_account_id', 'name', 'id' %>" 
-	end
+#  def index
+#    list
+#    render :action => 'list'
+#  end
+#
+#  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+#  verify :method => :post, :only => [ :destroy, :create, :update ],
+#         :redirect_to => { :action => :list }
+#
+#  def list
+#    @entry_pages, @entries = paginate :entries, :per_page => 10
+#  end
+#
+#  def show
+#    @entry = Entry.find(params[:id])
+#  end
+#
+#  def new
+#		#TODO: fetch fiscal period from somewhere
+#		@entry = Entry.find(:first, :order => "id desc", :conditions => ['fiscal_period_id = ?', 2006])
+#		@entry.id += 1
+#		@entry.receipt_number += 1
+#  end
+#
+#  def create
+#    @entry = Entry.new(params[:entry])
+#
+#    if @entry.save
+#      flash[:notice] = 'Entry was successfully created.'
+#      #redirect_to :action => 'list'
+#      redirect_to :action => 'new'
+#    else
+#      render :action => 'new'
+#    end
+#  end
+#
+#  def edit
+#    @entry = Entry.find(params[:id])
+#  end
+#
+#  def update
+#    @entry = Entry.find(params[:id])
+#    if @entry.update_attributes(params[:entry])
+#      flash[:notice] = 'Entry was successfully updated.'
+#      redirect_to :action => 'show', :id => @entry
+#    else
+#      render :action => 'edit'
+#    end
+#  end
+#
+#  def destroy
+#    Entry.find(params[:id]).destroy
+#    redirect_to :action => 'list'
+#  end
+#
+#	def autocomplete_debet_account
+#		data = params[:debet_account]
+#		@accounts = Account.find(:all,
+#				:conditions => [ 'number LIKE ? OR name LIKE ?', '%'+data+'%', '%'+data+'%'] )
+#		render :inline => "<%= indexed_auto_complete_result @accounts, 'entry_debet_account_id', 'name', 'id' %>" 
+#	end
+#
+#	def autocomplete_credit_account
+#		data = params[:credit_account]
+#		@accounts = Account.find(:all,
+#				:conditions => [ 'number LIKE ? OR name LIKE ?', '%'+data+'%', '%'+data+'%'] )
+#		render :inline => "<%= indexed_auto_complete_result @accounts, 'entry_credit_account_id', 'name', 'id' %>" 
+#	end
 
 end
--- app/models/fiscal_period.rb
+++ app/models/fiscal_period.rb
@@ -3,4 +3,9 @@ class FiscalPeriod < ActiveRecord::Base
   has_many  :budgets
   has_many  :accounts
   has_many  :invoices
+  
+  def to_s
+#    return "#{self.startdate} - #{self.enddate}"
+    return "#{self.id}"
+  end
 end
