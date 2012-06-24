dbe9b641264e244a1e302ff676f2ca444253739f
Anssi Kolehmainen <anssi.kolehmainen@iki.fi>
2012-06-24 14:15:33 +0300

    Fix pdf generation (=update rtex)

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,8 @@ Rails::Initializer.run do |config|
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
 
+  config.gem 'rtex'
+
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
   config.action_controller.session_store = :active_record_store
