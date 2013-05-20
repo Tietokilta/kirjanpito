50e32e875ba9405ade670e50a48475a37171fe53
Lauri Tirkkonen <lotheac@iki.fi>
2013-05-20 21:12:05 +0300

    use debian rails (2.3.14, backported patches)

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,6 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
-RAILS_GEM_VERSION = '2.3.15' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require 'thread'
